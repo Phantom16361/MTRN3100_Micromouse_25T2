e5736769b2a46aa268c03edb1d0011844049f0fe
Phantom16361 <2664698900jason@gmail.com>
2025-07-15 15:14:40 +1000

    Added EncoderOdometry.cpp

---
+++ Code/src/main_project/main/EncoderOdometry.cpp
@@ -0,0 +1,104 @@
+#include "EncoderOdometry.hpp"
+
+volatile long EncoderOdometry::leftTicks = 0;
+volatile long EncoderOdometry::rightTicks = 0;
+
+EncoderOdometry::EncoderOdometry(float wheelRadiusMM, float axleLengthMM, int ticksPerRevolution)
+    : radius(wheelRadiusMM),
+      axleLength(axleLengthMM),
+      ticksPerRev(ticksPerRevolution),
+      x(0.0), y(0.0), theta(0.0),
+      prevLeftTicks(0), prevRightTicks(0),
+      lastUpdateTime(0)
+{
+    mmPerTick = (2.0 * 3.14159 * radius) / ticksPerRev;
+}
+
+void EncoderOdometry::begin() {
+    pinMode(ENC1_A, INPUT);
+    pinMode(ENC1_B, INPUT);
+    pinMode(ENC2_A, INPUT);
+    pinMode(ENC2_B, INPUT);
+
+    attachInterrupt(digitalPinToInterrupt(ENC1_A), handleLeftA, RISING);
+    attachInterrupt(digitalPinToInterrupt(ENC2_A), handleRightA, RISING);
+
+    lastUpdateTime = millis();
+}
+
+void EncoderOdometry::reset() {
+    noInterrupts();
+    leftTicks = 0;
+    rightTicks = 0;
+    interrupts();
+
+    x = 0;
+    y = 0;
+    theta = 0;
+    prevLeftTicks = 0;
+    prevRightTicks = 0;
+    lastUpdateTime = millis();
+}
+
+void EncoderOdometry::update() {
+    long left, right;
+    noInterrupts();
+    left = leftTicks;
+    right = rightTicks;
+    interrupts();
+
+    long dLeft = left - prevLeftTicks;
+    long dRight = right - prevRightTicks;
+
+    prevLeftTicks = left;
+    prevRightTicks = right;
+
+    float dL_mm = dLeft * mmPerTick;
+    float dR_mm = dRight * mmPerTick;
+
+    float dCenter = (dL_mm + dR_mm) / 2.0;
+    float dTheta = (dR_mm - dL_mm) / axleLength;
+
+    theta += dTheta;
+    x += dCenter * cos(theta);
+    y += dCenter * sin(theta);
+
+    lastUpdateTime = millis();
+}
+
+float EncoderOdometry::getX() const { return x; }
+float EncoderOdometry::getY() const { return y; }
+float EncoderOdometry::getTheta() const { return theta; }
+
+long EncoderOdometry::getLeftTicks() const { return leftTicks; }
+long EncoderOdometry::getRightTicks() const { return rightTicks; }
+
+float EncoderOdometry::getLeftSpeedMMs() const {
+    long leftNow = leftTicks;
+    long deltaTicks = leftNow - prevLeftTicks;
+    unsigned long now = millis();
+    float dt = (now - lastUpdateTime) / 1000.0;
+    return (deltaTicks * mmPerTick) / dt;
+}
+
+float EncoderOdometry::getRightSpeedMMs() const {
+    long rightNow = rightTicks;
+    long deltaTicks = rightNow - prevRightTicks;
+    unsigned long now = millis();
+    float dt = (now - lastUpdateTime) / 1000.0;
+    return (deltaTicks * mmPerTick) / dt;
+}
+
+void EncoderOdometry::handleLeftA() {
+    if (digitalRead(ENC1_B))
+        leftTicks--;
+    else
+        leftTicks++;
+}
+
+void EncoderOdometry::handleRightA() {
+    if (digitalRead(ENC2_B))
+        rightTicks--;
+    else
+        rightTicks++;
+}
--- Code/src/main_project/main/main.ino
+++ Code/src/main_project/main/main.ino
@@ -1,37 +1,66 @@
 #include <Arduino.h>
-
-// Core modules
 #include "pin_config.hpp"
 #include "robot_param.hpp"
 #include "EncoderOdometry.hpp"
 
-// Create odometry object using constants from robot_param.hpp
+#include "pin_config.hpp"
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
+
+Adafruit_SSD1306 display(OLED_WIDTH, OLED_HEIGHT, &Wire, OLED_RESET);
+
 EncoderOdometry odom(WHEEL_RADIUS_MM, AXLE_LENGTH_MM, TICKS_PER_REV);
 
 void setup() {
   Serial.begin(115200);
-  delay(300);  // Give serial monitor time to connect
-
+  delay(300);
   Serial.println("Encoder Odometry Test Starting...");
   odom.begin();
+
+
+  // Start of OLED Setup
+  if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) {
+    Serial.println("OLED init failed");
+    while (true)
+      ;  // halt
+  }
+
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.println("Odometry OLED Ready");
+  display.display();
+  // End of OLED Setup
 }
 
 void loop() {
   odom.update();
 
-  // Print pose estimate
-  Serial.print("Ticks L: ");
-  Serial.print(odom.getLeftTicks());
-  Serial.print(" | R: ");
-  Serial.print(odom.getRightTicks());
+  // Serial.print("Ticks L: ");
+  // Serial.print(odom.getLeftTicks());
+  // Serial.print(" | R: ");
+  // Serial.print(odom.getRightTicks());
+  // Serial.print(" || X: ");
+  // Serial.print(odom.getX(), 1);
+  // Serial.print(" mm | Y: ");
+  // Serial.print(odom.getY(), 1);
+  // Serial.print(" mm | θ: ");
+  // Serial.print(odom.getTheta(), 2);
+  // Serial.println(" rad");
 
-  Serial.print(" || X: ");
-  Serial.print(odom.getX(), 1);
-  Serial.print(" mm | Y: ");
-  Serial.print(odom.getY(), 1);
-  Serial.print(" mm | θ: ");
-  Serial.print(odom.getTheta(), 2);
-  Serial.println(" rad");
+  // Start of OLED print
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.print("X: ");
+  display.print(odom.getX(), 0);
+  display.print("mm\nY: ");
+  display.print(odom.getY(), 0);
+  display.print("mm\nTh: ");
+  display.print(odom.getTheta(), 2);
+  display.print(" rad");
+  display.display();
+  // End of OLED print
 
-  delay(100);  // 10 Hz update rate
+  delay(100);
 }
